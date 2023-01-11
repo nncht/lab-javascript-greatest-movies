b8790e01d460d2f7f45a629b7892a4ea56b42f03
Nana Chuto <mail@nanachuto.com>
2023-01-11 01:00:27 +0100

    in progress

--- src/movies.js
+++ src/movies.js
@@ -1,10 +1,22 @@
 // Iteration 1: All directors? - Get the array of all directors.
+function getAllDirectors(moviesArray) {
+  const allDirectors = moviesArray.map((movies) => movies.director);
+  return allDirectors;
+  console.log(allDirectors);
+}
+
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors(moviesArray) {}
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(moviesArray) {}
+function howManyMovies(moviesArray) {
+  const onlySpielbergDramaMovies = moviesArray.filter((movies) => {
+    return (
+      movies.director === "Steven Spielberg" && movies.genre.includes("Drama")
+    );
+  });
+  return onlySpielbergDramaMovies.length;
+}
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
 function scoresAverage(moviesArray) {}
